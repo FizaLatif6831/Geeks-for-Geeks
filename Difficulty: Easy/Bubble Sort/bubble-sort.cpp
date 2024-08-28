850d328990adc24f762e0175532efbe24c884a32
FizaLatif6831 <fizalatif6831@gmail.com>
2024-08-28 23:27:42 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -22,15 +22,17 @@ class Solution {
     // Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n) {
         // Your code here
-         for (int i = 0; i < n - 1; i++) {
-        // Perform a pass over the array
-        for (int j = 0; j < n - i - 1; j++) {
-            // Swap if the element found is greater than the next element
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    swap(arr[j],arr[j+1]);
+                }
             }
         }
-    }
     }
 };
 
