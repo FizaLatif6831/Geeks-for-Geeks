32ed5246150373cf7f3d1e40a95fa29406f6fa16
FizaLatif6831 <fizalatif6831@gmail.com>
2024-08-28 23:33:26 +0500

    Update bubble-sort.cpp

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -22,6 +22,7 @@ class Solution {
     // Function to sort the array using bubble sort algorithm.
     void bubbleSort(int arr[], int n) {
         // Your code here
+        bool swaping=0;
         
         for(int i=n-1;i>=0;i--)
         {
@@ -30,7 +31,12 @@ class Solution {
                 if(arr[j]>arr[j+1])
                 {
                     swap(arr[j],arr[j+1]);
+                    swaping=1;
                 }
+            }
+            if(swaping == 0)
+            {
+                break;
             }
         }
     }
